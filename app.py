3934a00006d186c4be4dfa97285186f47fa26f6b
Riya-bhatu <riyabhatu05@gmail.com>
2026-02-06 11:20:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
-# Basic Calculator
+def add(a, b):
+    return a + b
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
 
-print("Choose operation:")
-print("1. Add (+)")
-print("2. Subtract (-)")
-print("3. Multiply (*)")
-print("4. Divide (/)")
+def subtract(a, b):
+    return a - b
 
-choice = input("Enter choice (1/2/3/4): ")
 
-if choice == "1":
-    print("Result:", num1 + num2)
-elif choice == "2":
-    print("Result:", num1 - num2)
-elif choice == "3":
-    print("Result:", num1 * num2)
-elif choice == "4":
-    if num2 != 0:
-        print("Result:", num1 / num2)
-    else:
-        print("Error: Division by zero")
-else:
-    print("Invalid choice")
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "cannot divide by zero"
+    return a / b
